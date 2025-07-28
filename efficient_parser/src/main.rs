48d11fb12c9199c02c5f3e28146d9e509b4df708
megahomyak <g.megahomyak@gmail.com>
2025-07-29 02:22:05 +0500

    Made a newer parser with a better interface, yet messed something up

--- efficient_parser/src/main.rs
+++ efficient_parser/src/main.rs
@@ -1,4 +1,7 @@
-use std::io::{Read, Seek, SeekFrom};
+use std::{
+    collections::HashMap,
+    io::{Read, Seek, SeekFrom},
+};
 
 mod parser;
 
@@ -23,10 +26,65 @@ impl<File: Seek + Read> parser::Cursor for FileCursor<File> {
     }
 }
 
+#[derive(Debug)]
+pub struct ParsingResult {
+    pub room_name: String,
+    pub server_ip: String,
+    pub username: String,
+    pub password: String,
+    pub new_message: Option<String>,
+}
+
+fn parse(mut line: parser::Line<impl parser::Cursor>) -> Option<ParsingResult> {
+    let mut config_kv = HashMap::new();
+    loop {
+        if let parser::LineCategory::Special() = line.categorize() {
+            let line_string: String = (&mut line).collect();
+            if let Some((command, arg)) = line_string.split_once('\\') {
+                if command == "config" {
+                    let (k, v) = arg.split_once('\\')?;
+                    config_kv.insert(k.to_owned(), v.to_owned());
+                }
+            }
+        } else {
+            break;
+        }
+        if !line.next_line() {
+            break;
+        }
+    }
+    line.last_line();
+    let mut new_message_lines = Vec::new();
+    loop {
+        if let parser::LineCategory::Regular() = line.categorize() {
+            let line_string: String = (&mut line).collect();
+            new_message_lines.push(line_string);
+        } else {
+            break;
+        }
+        if !line.prev_line() {
+            break;
+        }
+    }
+    let new_message = if new_message_lines.len() == 0 {
+        None
+    } else {
+        new_message_lines.reverse();
+        Some(new_message_lines.join("\n"))
+    };
+    Some(ParsingResult {
+        room_name: config_kv.remove("room name")?,
+        server_ip: config_kv.remove("server ip")?,
+        username: config_kv.remove("username")?,
+        password: config_kv.remove("password")?,
+        new_message,
+    })
+}
+
 fn main() {
     let chatfile_path = std::env::args().nth(1).unwrap();
-    let mut beginning = FileCursor {
+    let line = parser::Line::new(FileCursor {
         file: std::fs::File::open(chatfile_path).unwrap(),
-    };
-    println!("{:#?}", parser::parse(&mut beginning));
+    });
+    println!("{:#?}", parse(line));
 }
--- efficient_parser/src/parser.rs
+++ efficient_parser/src/parser.rs
@@ -1,166 +1,94 @@
-#[derive(Debug)]
-pub struct ParsingResult {
-    pub room_name: String,
-    pub server_ip: String,
-    pub username: String,
-    pub password: String,
-    pub new_message: Option<String>,
-}
-
 pub trait Cursor {
     fn back(&mut self) -> bool;
     fn read(&mut self) -> Option<char>;
     fn end(&mut self);
 }
 
-// starts at line beginning, ends at line beginning on success or wherever on error
-fn next_line(cursor: &mut impl Cursor) -> bool {
-    while let Some(c) = cursor.read() {
-        if c == '\n' {
-            return true;
-        }
-    }
-    false
+pub struct Line<C: Cursor> {
+    cursor: C,
 }
 
-// starts wherever, ends at line beginning
-fn line_beginning(cursor: &mut impl Cursor) {
-    while cursor.back() && cursor.read().unwrap() != '\n' {
-        cursor.back();
-    }
-}
-
-// starts at line beginning, ends at line beginning on success or wherever on error
-fn prev_line(cursor: &mut impl Cursor) -> bool {
-    if !cursor.back() {
-        return false;
-    }
-    line_beginning(cursor);
-    true
-}
-
-// starts wherever, ends after pattern on success or where started on error
-fn match_str(cursor: &mut impl Cursor, pattern: &'static str) -> bool {
-    let mut distance = 0;
-    'iteration: {
-        for c in pattern.chars() {
-            match cursor.read() {
-                Some(next) => {
-                    distance += 1;
-                    if next != c {
-                        break 'iteration;
-                    }
-                },
-                None => break 'iteration,
-            }
-        }
-        return true;
-    };
-    for _ in 0..distance {
-        cursor.back();
-    }
-    false
-}
-
-// starts wherever, ends in the same line at the line separator
-fn read_line(cursor: &mut impl Cursor) -> String {
-    let mut buffer = String::new();
-    while let Some(c) = cursor.read() {
-        if c == '\n' {
-            cursor.back();
-            break;
-        } else {
-            buffer.push(c);
-        }
-    }
-    buffer
-}
-
-enum LineCategory {
+pub enum LineCategory {
     Special(),
     Regular(),
 }
 
-// starts at line beginning, ends at line category's contents
-fn categorize_line(cursor: &mut impl Cursor) -> LineCategory {
-    if match_str(cursor, "\\\\") {
-        cursor.back();
-    } else if match_str(cursor, "\\") {
-        return LineCategory::Special();
+impl<C: Cursor> Line<C> {
+    pub fn new(cursor: C) -> Self {
+        Self { cursor }
     }
-    LineCategory::Regular()
+    fn back_real(&mut self) -> bool {
+        if self.cursor.back() {
+            let c = self.cursor.read().unwrap();
+            self.cursor.back();
+            if c == '\r' {
+                self.cursor.back()
+            } else {
+                true
+            }
+        } else {
+            false
+        }
+    }
+    fn read_real(&mut self) -> Option<char> {
+        match self.cursor.read() {
+            None => None,
+            Some(c) => if c == '\r' {
+                self.cursor.read()
+            } else {
+                Some(c)
+            }
+        }
+    }
+    fn line_beginning(&mut self) {
+        while self.cursor.back() && self.cursor.read().unwrap() != '\n' {
+            self.cursor.back();
+        }
+    }
+    pub fn prev_line(&mut self) -> bool {
+        if !self.back_real() {
+            return false;
+        }
+        self.line_beginning();
+        true
+    }
+    pub fn next_line(&mut self) -> bool {
+        while let Some(c) = self.read_real() {
+            if c == '\n' {
+                if self.cursor.read().is_some() {
+                    self.cursor.back();
+                    return true;
+                }
+            }
+        }
+        false
+    }
+    pub fn last_line(&mut self) {
+        self.cursor.end();
+        self.cursor.back();
+        self.line_beginning();
+    }
+    pub fn categorize(&mut self) -> LineCategory {
+        if self.read_real() == Some('\\') {
+            if self.read_real() == Some('\\') {
+                self.back_real();
+            } else {
+                return LineCategory::Special();
+            }
+        }
+        LineCategory::Regular()
+    }
 }
 
-// starts at chatfile beginning, ends wherever
-pub fn parse(beginning: &mut impl Cursor) -> Option<ParsingResult> {
-    let mut room_name = None;
-    let mut server_ip = None;
-    let mut username = None;
-    let mut password = None;
-    loop {
-        let mut line_read = false;
-        struct LineChecker<'a, C: Cursor> {
-            line_read: &'a mut bool,
-            cursor: &'a mut C,
-        }
-        impl<'a, C: Cursor> LineChecker<'a, C> {
-            fn check_line(&mut self, receiver: &mut Option<String>, pattern: &'static str) -> bool {
-                let matched = match_str(self.cursor, pattern);
-                if matched {
-                    *receiver = Some(read_line(self.cursor));
-                    self.cursor.read();
-                    *self.line_read = true;
-                }
-                matched
-            }
-        }
-        if let LineCategory::Special() = categorize_line(beginning) {
-            if match_str(beginning, "config\\") {
-                let mut line_reader = LineChecker { line_read: &mut line_read, cursor: beginning };
-                if line_reader.check_line(&mut room_name, "room name\\") {
-                } else if line_reader.check_line(&mut server_ip, "server ip\\") {
-                } else if line_reader.check_line(&mut username, "username\\") {
-                } else if line_reader.check_line(&mut password, "password\\") {
-                }
-            }
-        }
-        if !line_read && !next_line(beginning) {
-            break;
-        }
-    };
-    let room_name = room_name?;
-    let username = username?;
-    let password = password?;
-    let server_ip = server_ip?;
-    beginning.end();
-    let end = beginning;
-    line_beginning(end);
-    if !end.read().is_some() {
-        end.back();
+impl<C: Cursor> Iterator for Line<C> {
+    type Item = char;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.cursor.read().and_then(|c| if c == '\n' {
+            self.cursor.back();
+            None
+        } else {
+            Some(c)
+        })
     }
-    line_beginning(end);
-    let mut new_message_lines = Vec::new();
-    loop {
-        match categorize_line(end) {
-            LineCategory::Special() => break,
-            LineCategory::Regular() => new_message_lines.push(read_line(end)),
-        }
-        line_beginning(end);
-        if !prev_line(end) {
-            break;
-        }
-    }
-    new_message_lines.reverse();
-    let new_message = if new_message_lines.len() == 0 {
-        None
-    } else {
-        Some(new_message_lines.join("\n"))
-    };
-    Some(ParsingResult {
-        room_name,
-        username,
-        password,
-        server_ip,
-        new_message,
-    })
 }
