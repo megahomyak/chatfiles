cd6f106fa7c8ce20acf15fc4f69b9a020dc409e7
megahomyak <g.megahomyak@gmail.com>
2025-08-31 22:32:48 +0500

    I finally fixed the client script!

--- blabber/client/client.sh
+++ blabber/client/client.sh
@@ -2,7 +2,21 @@
 set -euo pipefail
 room_file_name="$1"
 room_file_path="$room_file_name"
-printf "\n" >> "$room_file_path" # For noeol files
-room_file_offset=$(( $(stat -c%s -- "$room_file_path") - $(tac -- "$room_file_path" | awk '/^\\[^\\]|^\\$/ {exit} {print}' | wc --bytes) ))
-truncate -s -1 -- "$room_file_path" # For noeol files
-dd "if=$room_file_path" skip=$room_file_offset iflag=skip_bytes status=none | curl -s -f --data-binary @- -H "X-Room-File-Offset: $room_file_offset" -H "X-Room-File-Name: $room_file_name" "$blabber_url" | tee -a -- "$room_file_path"
+new_message_offset="$(python -c '
+import sys, os
+with open(sys.argv[1], "rb") as f:
+    f.seek(0, os.SEEK_END)
+    consecutive_backslashes_count = 0
+    for i in range(f.tell()-1, -1, -1):
+        f.seek(i)
+        c = f.read(1)
+        if c == b"\n" and consecutive_backslashes_count == 1:
+            next(f)
+            break
+        elif c == b"\\":
+            consecutive_backslashes_count += 1
+        else:
+            consecutive_backslashes_count = 0
+    print(f.tell())
+' "$room_file_path")"
+dd "if=$room_file_path" skip=$new_message_offset iflag=skip_bytes status=none | curl -s -f --data-binary @- -H "X-Room-File-Offset: $new_message_offset" -H "X-Room-File-Name: $room_file_name" "$blabber_url" | tee -a -- "$room_file_path"
