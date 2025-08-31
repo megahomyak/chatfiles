5a302e6c0e1c5e957ed96c83e9b026bad2ac6ac1
megahomyak <g.megahomyak@gmail.com>
2025-08-31 13:36:17 +0500

    Fixes to tha client to make it support noeol situations

--- blabber/client/client.sh
+++ blabber/client/client.sh
@@ -2,6 +2,7 @@
 set -euo pipefail
 room_file_name="$1"
 room_file_path="$room_file_name"
-touch -- "$room_file_path"
+printf "\n" >> "$room_file_path"
 room_file_offset=$(( $(stat -c%s -- "$room_file_path") - $(tac -- "$room_file_path" | awk '/^\\[^\\]|^\\$/ {exit} {print}' | wc --bytes) ))
+truncate -s -1 -- "$room_file_path"
 dd "if=$room_file_path" skip=$room_file_offset iflag=skip_bytes status=none | curl -s -f --data-binary @- -H "X-Room-File-Offset: $room_file_offset" -H "X-Room-File-Name: $room_file_name" "$blabber_url" | tee -a -- "$room_file_path"
