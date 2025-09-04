95c0de0046f15b7e2f016e3619f1b284f8930753
megahomyak <g.megahomyak@gmail.com>
2025-09-05 03:59:25 +0500

    What the fuck is wrong with the request body...

--- blabber/client/client.py
+++ blabber/client/client.py
@@ -1,17 +1,17 @@
 import sys, os, fcntl, urllib.request, base64, urllib.parse
 room_file_path = sys.argv[1]
 with open(room_file_path, "a+b") as room_file:
-    fcntl.flock(room_file, flock.LOCK_EX)
+    fcntl.flock(room_file, fcntl.LOCK_EX)
     for i in range(room_file.tell()-1, -1, -1):
         room_file.seek(i)
         chunk = room_file.read(2)
         if chunk == b"\n\\" or (i == 0 and chunk.startswith(b"\\")):
             f.readline()
             break
-    room_file_offset = room_file.tell()
-    parse_result = urllib.parse(os.environ["blabber_url"])
+    new_message_offset = room_file.tell()
+    parse_result = urllib.parse.urlparse(os.environ["blabber_url"])
     creds, real_netloc = parse_result.netloc.split("@")
-    response = urllib.request.urlopen(urllib.request.Request("{scheme}://{real_netloc}/{room_file_name}".format(scheme=parse_result.scheme, real_netloc=real_netloc, room_file_name=os.path.basename(room_file_path)}), data=room_file, headers={"Range": "bytes={}-".format(new_message_offset), "Authorization": b"Basic " + base64.b64encode(creds)}))
-    for chunk in iter(lambda: response.read(4096), b""):
-        sys.stdout.buffer.write(chunk)
-        room_file.write(chunk)
+    with urllib.request.urlopen(urllib.request.Request("{scheme}://{real_netloc}/{room_file_name}".format(scheme=parse_result.scheme, real_netloc=real_netloc, room_file_name=os.path.basename(room_file_path)), data=room_file, headers={"Range": "bytes={}-".format(new_message_offset), "Authorization": b"Basic " + base64.b64encode(creds.encode())})) as response:
+        for chunk in iter(lambda: response.read(4096), b""):
+            sys.stdout.buffer.write(chunk)
+            room_file.write(chunk)
--- blabber/server/server.py
+++ blabber/server/server.py
@@ -1,22 +1,33 @@
 #!/usr/bin/python3
-import os, base64, fcntl, http.server, hashlib, datetime, shutil
-password_lines = open(os.environ["blabber_credfile"]).read().splitlines()
-class Handler(http.server.BaseHTTPRequestHandler):
-    def do_POST(self):
-        username, password = base64.b64decode(self.headers["Authorization"].split("Basic", 1)[1]).decode().split(":")
-        assert "{username}:{password_hash}".format(username=username, password_hash=hashlib.sha256(password.encode()).hexdigest()) in password_lines
-        room_file_offset = int(self.headers["Range"].split("bytes=", 1)[1].split("-")[0])
-        room_file_path = os.path.basename(self.path.split("/", 1)[1])
-        new_message = self.rfile.read()
-        assert all(not line.startswith(b"\\") for line in new_message.splitlines())
-        self.send_response(200)
-        self.end_headers()
-        footer = "\n\\{username} @ {datetime}\n".format(username=username, datetime=datetime.datetime.now(datetime.timezone.utc)).encode()
-        if new_message: self.wfile.write(footer)
-        with open(room_file_path, "a+" if new_message else "r") as room_file: # This may throw, that's intended
-            fcntl.flock(room_file, flock.LOCK_EX)
-            if new_message: self.wfile.write(footer)
-            room_file.seek(room_file_offset)
-            shutil.copyfileobj(room_file_path, self.wfile)
-            if new_message: room_file_path.write(new_message + footer)
-http.server.HTTPServer((os.environ["blabber_host"], int(os.environ["blabber_port"])), Handler).serve_forever()
+import os, base64, fcntl, http.server, hashlib, datetime, shutil, sys, getpass
+with open(os.environ["blabber_credfile"], "a+") as credfile:
+    if sys.argv[1] == "serve":
+        credfile.seek(0)
+        password_lines = credfile.read().splitlines()
+        class Handler(http.server.BaseHTTPRequestHandler):
+            def do_POST(self):
+                username, password = base64.b64decode(self.headers["Authorization"].split("Basic", 1)[1].lstrip()).decode().split(":")
+                assert "{username}:{password_hash}".format(username=username, password_hash=hashlib.sha256(password.encode()).hexdigest()) in password_lines
+                room_file_offset = int(self.headers["Range"].split("bytes=", 1)[1].split("-")[0])
+                room_file_path = os.path.basename(self.path.split("/", 1)[1])
+                print(1)
+                while True:
+                    print(self.rfile.read(1))
+                new_message = self.rfile.read()
+                print(2)
+                assert all(not line.startswith(b"\\") for line in new_message.splitlines())
+                self.send_response(200)
+                self.end_headers()
+                footer = "\n\\{username} @ {datetime}\n".format(username=username, datetime=datetime.datetime.now(datetime.timezone.utc)).encode()
+                if new_message: self.wfile.write(footer)
+                with open(room_file_path, "a+" if new_message else "r") as room_file: # This may throw, that's intended
+                    fcntl.flock(room_file, flock.LOCK_EX)
+                    if new_message: self.wfile.write(footer)
+                    room_file.seek(room_file_offset)
+                    shutil.copyfileobj(room_file_path, self.wfile)
+                    if new_message: room_file_path.write(new_message + footer)
+        host, port = os.environ["blabber_bind"].split(":")
+        http.server.HTTPServer((host, int(port)), Handler).serve_forever()
+    elif sys.argv[1] == "add_user":
+        credfile.write("{username}:{password}".format(username=sys.argv[2], password=hashlib.sha256(getpass.getpass().encode()).hexdigest()))
+    else: raise Exception("Unknown command \"{}\"".format(sys.argv[1]))
