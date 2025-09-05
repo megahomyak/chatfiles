0a275d11da88290d2527fe42db33777658bd10b9
megahomyak <g.megahomyak@gmail.com>
2025-09-05 19:49:38 +0500

    All stuff is fixed now, I guess

--- blabber/advanced_usage/client/shortcuts/bash.sh
+++ blabber/advanced_usage/client/shortcuts/bash.sh
@@ -2,8 +2,8 @@ blabber_flags="blabber_username=USERNAME blabber_password=PASSWORD blabber_url=U
 blabber_repo="REPO_PATH"
 blabber_device="pc OR termux"
 blabu() {
-    $blabber_flags "$blabber_repo/client.py" "$1"
+    env $blabber_flags "$blabber_repo/client.py" "$1"
 }
 blabn() {
-    $blabber_flags "$blabber_repo/advanced_usage/client/notifiers/notify-$blabber_device" "$1"
+    env $blabber_flags "$blabber_repo/advanced_usage/client/notifiers/notify-$blabber_device" "$1"
 }
--- blabber/client.py
+++ blabber/client.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python3
-import sys, os, fcntl, urllib.request, json
+import sys, os, fcntl, urllib.request, json, io
 room_file_path = sys.argv[1]
-with open(room_file_path, "a+", encoding="utf-8") as room_file:
+with open(room_file_path, "a+b") as room_file:
     fcntl.flock(room_file, fcntl.LOCK_EX)
     for i in range(room_file.tell()-1, -1, -1):
         room_file.seek(i)
         chunk = room_file.read(2)
-        if chunk == "\n\\" or (i == 0 and chunk.startswith("\\")):
+        if chunk == b"\n\\" or (i == 0 and chunk.startswith(b"\\")):
             room_file.readline()
             break
         else:
             room_file.seek(i)
     new_message_offset = room_file.tell()
-    with urllib.request.urlopen(urllib.request.Request(os.environ["blabber_url"], data=(json.dumps({"offset": new_message_offset, "msg": room_file.read(), "room": os.path.basename(room_file_path), "password": os.environ["blabber_password"], "username": os.environ["blabber_username"]}) + "\n").encode("utf-8"))) as response:
-        for chunk in iter(lambda: response.read(4096), b""):
-            sys.stdout.buffer.write(chunk)
-            room_file.write(chunk.decode("utf-8"))
+    with io.TextIOWrapper(room_file, encoding="utf-8") as room_file:
+        with io.TextIOWrapper(urllib.request.urlopen(urllib.request.Request(os.environ["blabber_url"], data=(json.dumps({"offset": new_message_offset, "msg": room_file.read(), "room": os.path.basename(room_file_path), "password": os.environ["blabber_password"], "username": os.environ["blabber_username"]}) + "\n").encode("utf-8"))), encoding="utf-8") as response:
+            for chunk in iter(lambda: response.read(4096), ""):
+                sys.stdout.write(chunk)
+                room_file.write(chunk)
