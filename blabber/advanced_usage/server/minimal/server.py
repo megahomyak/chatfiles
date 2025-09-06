027dde83654bf6580456db308f588164991021f6
megahomyak <g.megahomyak@gmail.com>
2025-09-06 23:17:43 +0500

    Included a minimal version of the server

---
+++ blabber/advanced_usage/server/minimal/server.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import os, fcntl, http.server, hashlib, datetime, shutil, json
+password_lines = set(open(os.environ["blabber_credfile"], "r", encoding="utf-8").read().splitlines())
+class Handler(http.server.BaseHTTPRequestHandler):
+    def do_POST(self):
+        req = json.loads(self.rfile.readline())
+        assert "{username}:{password_hash}".format(username=req["username"], password_hash=hashlib.sha256(req["password"].encode("utf-8")).hexdigest()) in password_lines
+        assert req["room"] == os.path.basename(req["room"])
+        assert all(not line.startswith("\\") for line in req["msg"].splitlines())
+        self.send_response(200)
+        self.end_headers()
+        footer = "\n\\{username} @ {datetime}\n".format(username=req["username"], datetime=datetime.datetime.now(datetime.timezone.utc)).encode("utf-8")
+        if req["msg"]: self.wfile.write(footer)
+        with open(req["room"], "a+b" if req["msg"] else "rb") as room_file: # This may throw, that's intended
+            fcntl.flock(room_file, fcntl.LOCK_EX)
+            room_file.seek(req["offset"])
+            shutil.copyfileobj(room_file, self.wfile)
+            if req["msg"]: room_file.write(req["msg"].encode("utf-8") + footer)
+http.server.HTTPServer((os.environ["blabber_host"], int(os.environ["blabber_port"])), Handler).serve_forever()
