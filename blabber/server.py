b8623ee5411c73b2823b1d9b178fb79a576f31e2
megahomyak <g.megahomyak@gmail.com>
2025-09-08 00:07:04 +0500

    Making more adequate HTTP responses for error cases, I guess

--- blabber/server.py
+++ blabber/server.py
@@ -5,22 +5,29 @@ with open(os.environ["blabber_credfile"], "a+b") as credfile:
         credfile.seek(0)
         password_lines = set(credfile.read().splitlines())
         class Handler(http.server.BaseHTTPRequestHandler):
-            def do_POST(self):
-                msg = self.rfile.read(int(self.headers["Content-Length"]))
-                username, password = base64.b64decode(self.headers["Authorization"].split("Basic ", 1)[1]).split(b":")
-                room_name = self.path.split("/rooms/", 1)[1]
-                assert username + b":" + hashlib.sha256(password).hexdigest().encode("ascii") in password_lines
-                assert room_name == os.path.basename(room_name)
-                assert all(not line.startswith(b"\\") for line in msg.splitlines())
-                self.send_response(200)
+            def send_response_with_headers(self, code):
+                self.send_response(code)
                 self.end_headers()
-                footer = b"\n\\" + username + b" @ " + str(datetime.datetime.now(datetime.timezone.utc)).encode("ascii") + b"\n"
-                if msg: self.wfile.write(footer)
-                with open(room_name, "a+b" if msg else "rb") as room_file: # This may throw, that's intended
-                    fcntl.flock(room_file, fcntl.LOCK_EX)
-                    room_file.seek(int(self.headers["Range"].split("bytes=", 1)[1].split("-", 1)[0]))
-                    shutil.copyfileobj(room_file, self.wfile)
-                    if msg: room_file.write(msg + footer)
+            def do_POST(self):
+                try:
+                    msg = self.rfile.read(int(self.headers["Content-Length"]))
+                    username, password = base64.b64decode(self.headers["Authorization"].split("Basic ", 1)[1]).split(b":")
+                    room_name = self.path.split("/rooms/", 1)[1]
+                    assert username + b":" + hashlib.sha256(password).hexdigest().encode("ascii") in password_lines
+                    assert room_name == os.path.basename(room_name)
+                    assert all(not line.startswith(b"\\") for line in msg.splitlines())
+                    self.send_response_with_headers(200)
+                    footer = b"\n\\" + username + b" @ " + str(datetime.datetime.now(datetime.timezone.utc)).encode("ascii") + b"\n"
+                    if msg: self.wfile.write(footer)
+                    try:
+                        with open(room_name, "a+b" if msg else "rb") as room_file:
+                            fcntl.flock(room_file, fcntl.LOCK_EX)
+                            room_file.seek(int(self.headers["Range"].split("bytes=", 1)[1].split("-", 1)[0]))
+                            shutil.copyfileobj(room_file, self.wfile)
+                            if msg: room_file.write(msg + footer)
+                    except FileNotFoundError: pass
+                except AssertionError: self.send_response_with_headers(400)
+                except: self.send_response_with_headers(500)
         http.server.HTTPServer((os.environ["blabber_host"], int(os.environ["blabber_port"])), Handler).serve_forever()
     elif sys.argv[1] == "add_user":
         credfile.write("{username}:{password}\n".format(username=sys.argv[2], password=hashlib.sha256(getpass.getpass().encode("utf-8")).hexdigest()).encode("utf-8"))
