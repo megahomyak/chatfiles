4bcb17f078a13a91bfc8f6e37282b3eb2e5d1cf8
megahomyak <g.megahomyak@gmail.com>
2025-09-20 02:58:45 +0500

    Made REALLY NICE errors!

--- blabber/client.py
+++ blabber/client.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-import sys, os, urllib.request, io, base64
+import sys, os, urllib.request, urllib.error, io, base64
 room_file_path = sys.argv[1]
 with open(room_file_path, "a+b") as room_file:
     end_offset = room_file.tell()
@@ -12,7 +12,10 @@ with open(room_file_path, "a+b") as room_file:
         else:
             room_file.seek(i)
     new_message_offset = room_file.tell()
-    with urllib.request.urlopen(urllib.request.Request(os.environ["blabber_url"] + "/rooms/" + os.path.basename(room_file_path), headers={"Range": "bytes={}-".format(new_message_offset), "Content-Length": end_offset - new_message_offset, "Authorization": b"Basic " + base64.b64encode(os.environb[b"blabber_creds"])}, data=room_file)) as response:
-        for chunk in iter(lambda: response.read(4096), b""):
-            sys.stdout.buffer.write(chunk)
-            room_file.write(chunk)
+    try:
+        with urllib.request.urlopen(urllib.request.Request(os.environ["blabber_url"] + "/rooms/" + os.path.basename(room_file_path), headers={"Range": "bytes={}-".format(new_message_offset), "Content-Length": end_offset - new_message_offset, "Authorization": b"Basic " + base64.b64encode(os.environb[b"blabber_creds"])}, data=room_file)) as response:
+            for chunk in iter(lambda: response.read(4096), b""):
+                sys.stdout.buffer.write(chunk)
+                room_file.write(chunk)
+    except urllib.error.HTTPError as error:
+        raise Exception(str(error.code) + " : " + error.reason + " : " + error.file.read().decode())
--- blabber/server.py
+++ blabber/server.py
@@ -1,25 +1,49 @@
 #!/usr/bin/env python3
-import os, http.server, hashlib, datetime, shutil, sys, getpass, base64
+import os, http.server, hashlib, datetime, shutil, sys, getpass, base64, re
 with open(os.environ["blabber_credfile"], "a+b") as credfile:
     if sys.argv[1] == "serve":
         credfile.seek(0)
         password_lines = set(credfile.read().splitlines())
+        fullmatch = lambda pattern, input_: re.match("^" + pattern + "$", input_, re.DOTALL).groups()
         class Handler(http.server.BaseHTTPRequestHandler):
-            def do_POST(self):
-                msg = self.rfile.read(int(self.headers["Content-Length"]))
-                username, password = base64.b64decode(self.headers["Authorization"].split("Basic ", 1)[1]).split(b":")
-                room_name = self.path.split("/rooms/", 1)[1]
-                assert username + b":" + hashlib.sha256(password).hexdigest().encode("ascii") in password_lines
-                assert room_name == os.path.basename(room_name)
-                assert all(not line.startswith(b"\\") for line in msg.splitlines())
-                self.send_response(200)
+            def _begin_response_body(self, code, code_name, headers=()):
+                self.send_response(code, code_name)
+                for header in headers:
+                    self.send_header(*header)
                 self.end_headers()
-                footer = b"\n\\" + username + b" @ " + str(datetime.datetime.now(datetime.timezone.utc)).encode("ascii") + b"\n"
-                if msg: self.wfile.write(footer)
-                with open(room_name, "a+b" if msg else "rb") as room_file: # This may throw, that's intended
-                    room_file.seek(int(self.headers["Range"].split("bytes=", 1)[1].split("-", 1)[0]))
-                    shutil.copyfileobj(room_file, self.wfile)
-                    if msg: room_file.write(msg + footer)
+            def do_POST(self):
+                class UserError(Exception): pass
+                class UserErrorChecker:
+                    def __init__(self, code, code_name, error_message, headers=()):
+                        self.code = code
+                        self.code_name = code_name
+                        self.error_message = error_message.encode("ascii")
+                        self.headers = headers
+                    def __enter__(self): pass
+                    def __exit__(checker_self, exc_type, *_):
+                        if exc_type is not None:
+                            self._begin_response_body(checker_self.code, checker_self.code_name, checker_self.headers)
+                            self.wfile.write(checker_self.error_message)
+                            raise UserError()
+                make_unauthorized_error_checker = lambda error_message: UserErrorChecker(401, "Unauthorized", error_message, [("WWW-Authenticate", 'Basic realm="Blabber", charset="UTF-8"')])
+                try:
+                    with UserErrorChecker(411, "Length Required", "could not read the body by Content-Length"): msg = self.rfile.read(int(self.headers["Content-Length"]))
+                    with make_unauthorized_error_checker("syntactically valid basic authorization not provided"): username, password = base64.b64decode(fullmatch(r"Basic (.+)", self.headers["Authorization"])[0]).split(b":")
+                    with UserErrorChecker(400, "Bad Request", "invalid room path"): room_name = fullmatch(r"/rooms/(.+)", self.path)[0]
+                    with make_unauthorized_error_checker("credentials not known"): assert username + b":" + hashlib.sha256(password).hexdigest().encode("ascii") in password_lines
+                    with UserErrorChecker(400, "Bad Request", "invalid room file name"): assert room_name == os.path.basename(room_name)
+                    with UserErrorChecker(400, "Bad Request", "new message contains special lines"): assert all(not line.startswith(b"\\") for line in msg.splitlines())
+                    with UserErrorChecker(400, "Bad Request", "invalid range header"): room_internal_offset = int(fullmatch(r"bytes=(\d+)-", self.headers["Range"])[0])
+                    self._begin_response_body(200, "OK")
+                    footer = b"\n\\" + username + b" @ " + str(datetime.datetime.now(datetime.timezone.utc)).encode("ascii") + b"\n"
+                    if msg: self.wfile.write(footer)
+                    try:
+                        with open(room_name, "a+b" if msg else "rb") as room_file:
+                            room_file.seek(room_internal_offset)
+                            shutil.copyfileobj(room_file, self.wfile)
+                            if msg: room_file.write(msg + footer)
+                    except FileNotFoundError: pass
+                except UserError: pass
         http.server.HTTPServer((os.environ["blabber_host"], int(os.environ["blabber_port"])), Handler).serve_forever()
     elif sys.argv[1] == "add_user":
         credfile.write("{username}:{password}\n".format(username=sys.argv[2], password=hashlib.sha256(getpass.getpass().encode("utf-8")).hexdigest()).encode("utf-8"))
